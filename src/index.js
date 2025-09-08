76a00fb1cf5a3f26865616be9e30ca579b8bf9c4
abhshek-04 <ak5608741@gmail.com>
2025-09-08 23:36:37 +0530

    connect to mongo atlas

---
+++ src/index.js
@@ -0,0 +1,51 @@
+import dotenv from 'dotenv'
+import connectDB from "./db/index.js";
+
+
+dotenv.config();
+
+
+
+
+
+connectDB()
+
+
+
+
+
+
+
+// const connectDB(){}
+// connectDB()
+
+
+
+
+// best way for begnner
+
+
+
+// import express from "express"
+
+// const app = express()
+
+
+/*
+( async ()=>{
+    try {
+        await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`)
+        app.on("error" , (error)=>{
+            console.log("error:", error)
+            throw error
+        })
+
+        app.listen(process.env.PORT) , ()=>{
+            console.log(`App is listening on port ${process.env.PORT}`);
+        }
+    } catch (error) {
+        console.log("error:", error)
+        throw err
+    }
+})()
+*/
